bc8abfe9cb8061d964bf2a367d8ce17cdb69af73
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2026-07-25 17:28:00 +0700

    Update eval_model.py

--- src/evaluattion/eval_model.py
+++ src/evaluattion/eval_model.py
@@ -1,23 +1,35 @@
+from omegaconf import DictConfig
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 from sklearn.metrics import confusion_matrix
 
+from src.common.utils import save_figure
+from src.common.utils_logging import logger
 
-for name, model in models.items():
-  model.fit(x_train, y_train)
-  y_pred = model.predict(x_test)
-  cm = confusion_matrix(y_test, y_pred)
-  plt.figure(figsize=(8, 6))
-  sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
-  plt.title(f'Confusion Matrix - {name}')
-  plt.xlabel('Predicted Labels')
-  plt.ylabel('True Labels')
-  plt.show()
+def plot_confusion_matrix(results, y_test: pd.DataFrame, cfg: DictConfig):
+  for model_key, result in results.items():
+    model_name = cfg.models[model_key].name
+    
+    y_pred = results[model_key]["y_pred"]
+    cm = confusion_matrix(y_test, y_pred)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, ax=ax)
+    ax.set_title(f'Confusion Matrix - {model_name}')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    save_figure(fig, cfg, "confusion_matrix", f"Confusion Matrix - {model_name}.png")
 
+  
 # Xuất ra điểm của các mô hình dự đoán
-for name, model in models.items():
-  train_score = model.score(x_train, y_train) * 100
-  test_score = model.score(x_test, y_test) * 100
-  print(f"{name}: \n Train Score: {train_score:.2f} %\n Test Score: {test_score:.2f} %") # Xuất điểm kết quả của các mô hình ra màn hình
-  print("-" * 30)
+def score_of_model(results, x_train: pd.DataFrame, y_train: pd.DataFrame, 
+                     x_test: pd.DataFrame, y_test: pd.DataFrame, cfg: DictConfig):
+    for model_key, model_cfg in results.items():
+      model_name = cfg.models[model_key].name
+      model = results[model_key]["model"]
+      train_score = model.score(x_train, y_train) * 100
+      test_score = model.score(x_test, y_test) * 100
+      logger.info(f"{model_name}: \n Train Score: {train_score:.2f} %\n Test Score: {test_score:.2f} %") # Xuất điểm kết quả của các mô hình ra màn hình
+      print("-" * 30)
